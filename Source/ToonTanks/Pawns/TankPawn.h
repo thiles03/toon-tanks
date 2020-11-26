13cacf157d6c99dff5f085c6ef7f390c7ffded02
thiles03 <thiles03@gmail.com>
2020-11-26 22:30:38 +0000

    Mesh edits

---
+++ Source/ToonTanks/Pawns/TankPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pawns/BasePawn.h"
+#include "TankPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATankPawn : public ABasePawn
+{
+	GENERATED_BODY()
+	
+};
