7622780d626f81384e12523c2ec3340f7ef7b495
thiles03 <thiles03@gmail.com>
2020-11-30 21:29:01 +0000

    Update TurretPawn.h

--- Source/ToonTanks/Pawns/TurretPawn.h
+++ Source/ToonTanks/Pawns/TurretPawn.h
@@ -10,8 +10,12 @@ class TOONTANKS_API ATurretPawn : public ABasePawn
 	GENERATED_BODY()
 
 public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
 protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 private:
 	
