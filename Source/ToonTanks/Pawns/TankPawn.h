c1d603aef988f12fa79968afbcec9e5a4f870e5f
thiles03 <thiles03@gmail.com>
2020-11-30 21:12:15 +0000

    Update TankPawn.h

--- Source/ToonTanks/Pawns/TankPawn.h
+++ Source/ToonTanks/Pawns/TankPawn.h
@@ -32,8 +32,9 @@ private:
 
 	FVector MoveDirection;
 	FQuat RotationDirection;
-
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))	
 	float MoveSpeed = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float RotateSpeed = 100.f;
 
 	void CalculateMoveInput(float Value);
