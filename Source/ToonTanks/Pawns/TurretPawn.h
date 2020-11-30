3b5d86aa94c30c40f453d8e69c37c914ceb14e03
thiles03 <thiles03@gmail.com>
2020-11-30 21:22:18 +0000

    Turret class added

---
+++ Source/ToonTanks/Pawns/TurretPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pawns/BasePawn.h"
+#include "TurretPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATurretPawn : public ABasePawn
+{
+	GENERATED_BODY()
+	
+};
