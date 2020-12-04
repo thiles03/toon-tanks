afc99d2130362079f654e39dde9007ea0f157fd8
thiles03 <thiles03@gmail.com>
2020-12-04 17:15:33 +0000

    Player controller

---
+++ Source/ToonTanks/PlayerControllers/PlayerControllerBase.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "PlayerControllerBase.h"
+
---
+++ Source/ToonTanks/PlayerControllers/PlayerControllerBase.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
